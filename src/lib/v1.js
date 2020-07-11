f3e59069a26f4ed687dc3cea9f4ce42a9ad4c3b1
EsraaMamoun <esraamamoun5@gmail.com>
2020-07-11 03:51:35 +0300

    done

--- src/lib/v1.js
+++ src/lib/v1.js
@@ -26,9 +26,9 @@ function getModel(req, res, next) {
 
 router.param('model', getModel);
 
-router.get('/:model', getAllFunction);
-router.get('/:model/:id', getOneFunction);
-router.post('/:model', postFunction);
+router.get('/:model', permissions('read'), getAllFunction);
+router.get('/:model/:id', permissions('read'), getOneFunction);
+router.post('/:model', permissions('read'), postFunction);
 router.put('/:model/:id', bearerMiddleware, permissions('update'), putFunction);
 router.delete('/:model/:id', bearerMiddleware, permissions('delete'), deleteFunction);
 
